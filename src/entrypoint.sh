ccab2921235d76c2823f31128c7179c030f668f5
naman <naman.nag@codemonk.in>
2024-01-24 12:11:44 +0530

    feat: Docker env done

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,15 +1,7 @@
-set -e
+echo 'Running migrations...'
+python manage.py migrate
 
-mkdir -p /src/logs
+echo 'Collecting static files...'
+python manage.py collectstatic --no-input
 
-cd /src
-
-export DJANGO_SETTINGS_MODULE=config.settings
-
-exec gunicorn config.wsgi:application \
-    --name config \
-    --reload \
-    --bind 0.0.0.0:8000 \
-    --workers 4 \
-    --log-level=debug
-    "$@"
+exec "$@"
